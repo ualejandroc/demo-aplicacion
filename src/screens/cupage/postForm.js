e10e6617a5c62e361870e0f0b548a710b8a74445
ualejandroc <ualejandroc@hotmail.com>
2018-06-18 23:04:41 -0500

    Correciones en api

--- src/screens/cupage/postForm.js
+++ src/screens/cupage/postForm.js
@@ -14,7 +14,8 @@ import {
   Icon,
   Form,
   Text,
-  Textarea
+  Textarea,
+  Dimensions, TouchableHighlight, Image
 } from "native-base";
 
 import Api from "../../products/WooCommerce/Woocommerce";
@@ -23,29 +24,32 @@ import styles from "../form/styles";
 
 import  Captures  from "./captures";
 
+GLOBAL.fetch = fetch;
+
+/************ */
+
+/************ */
+
 class PostForm extends Component {
 
- /* takePicture() {
-    this.camera.capture()
-       .then((data) => console.log(data))
-       .catch(err => console.error(err));
-  }*/
+
   state = {
+    card: {pic:''},
        resp:'',
-    dataModel :{
+ 
       name: 'Premium Quality',
   type: 'simple',
-  regular_price: '21.99',
+  regular_price: '45.99',
   description: 'Pellentesque habitant  tristiqueo.',
   short_description: 'Pellentesque habitant senectus et netus et malesuada fames ac turpis egestas.',
-  categories: [
+ /* categories: [
     {
       id: 9
     },
     {
       id: 14
     }
-  ],
+  ],*/
   images: [
     {
       src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg',
@@ -55,29 +59,81 @@ class PostForm extends Component {
       src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg',
       position: 1
     }
-  ]
-    }  
+  ],
+  dataModel:''
+    
   };
 
-   
-  fetchData() {
-
-    
+  buildModel(){
+   this.setState({
+     dataModel:  //JSON.stringify(
+      `name=${encodeURI(this.state.name)}&`+
+       `type=${encodeURI(this.state.type)}&`+
+       `regular_price=${ this.state.regular_price}&`+
+       `short_description=${ this.state.short_description}&`+
+      `description=${encodeURI(this.state.description)}&`+
+      `images=${JSON.stringify(this.state.images)}`
+    //  )
+     }) ;
   }
- 
-  savePost(datas){
-    var self =this;
-    Api.post('products', datas, function(err, data, res) {
-    console.log(res);
-    
-    self.setState({resp:err });
-    
-    }).then(function (datax) {
-      console.log(datax);
 
-     
+   
+  fetchDatas(){
+    var self = this;
+    var data = "username=acceso&password=0995480563";
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        self.fillText(JSON.parse(this.responseText).token);
+        
+      }
     });
-   }
+    
+    xhr.open("POST", "https://crearstore.com/wp-json/jwt-auth/v1/token");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.setRequestHeader("postman-token", "f44d488a-4ee4-a32c-1324-fad1c2d837ca");
+    
+    xhr.send(data);
+  }
+
+
+  fillText(token){
+    var self = this;
+    //var data = "name=remium%20Quality&type=simple&regular_price=29&description=description&short_description=short_description";
+    self.buildModel();
+    var data=self.state.dataModel;
+
+    console.log(self.state.dataModel);
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        self.setState({
+          card: { pic: JSON.stringify(this.responseText) }
+          //card: { pic: responseJson[0].guid.rendered }
+        });
+      }
+    });
+    
+    xhr.open("POST", "http://crearstore.com/wp-json/wc/v2/products");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("authorization", "Bearer "+ token);
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.setRequestHeader("postman-token", "4c654d71-858b-54a9-280a-819ffb6155e5");
+    
+    xhr.send(data);
+  }
+
+ 
+ 
 
 
   render() {
@@ -100,7 +156,7 @@ class PostForm extends Component {
             <Item inlineLabel>
               <Label>Titulo de Producto</Label>              
             </Item>
-            <Input  onChangeText={(text) => this.setState({dataModel:{name: text}})} />
+            <Input  onChangeText={(text) => this.setState({name: text})} />
 
             <Text style={styles.separation}></Text>       
           <Text style={styles.separation}></Text>
@@ -111,7 +167,7 @@ class PostForm extends Component {
              <Item inlineLabel>
               <Label>Precio</Label>              
             </Item>
-            <Input  onChangeText={(text) => this.setState({dataModel:{regular_price: text}})} />
+            <Input  onChangeText={(text) => this.setState({regular_price: text})} />
 
             <Text style={styles.separation}></Text>           
 
@@ -120,7 +176,7 @@ class PostForm extends Component {
               <Label               
               floatingLabel >Descripcion corta</Label>              
               <Textarea rowSpan={5}
-              onChangeText={(text) => this.setState({dataModel:{short_description: text}})} 
+              onChangeText={(text) => this.setState({short_description: text})} 
               bordered 
               info placeholder="..." />
             
@@ -131,7 +187,7 @@ class PostForm extends Component {
               <Label               
               floatingLabel >Descripcion Detallada</Label>              
               <Textarea rowSpan={5} 
-             onChangeText={(text) => this.setState({dataModel:{description: text}})} 
+             onChangeText={(text) => this.setState({description: text})} 
             bordered info placeholder="..." />
 
               <Text style={styles.separation}></Text>
@@ -154,13 +210,17 @@ class PostForm extends Component {
             */}
           </Form>
           <Button block 
-          onPress={() => {this.savePost(this.state.dataModel ); } }
+
+          underlayColor='#ccc'
+          onPress={() => { this.fetchDatas( ); } }
           style={{ margin: 15, marginTop: 50 }}
           >
             <Text>Guardar</Text>
           </Button>
-          <Text style={styles.separation}>{JSON.stringify(this.state.dataModel)}</Text> 
+          <Text style={styles.separation}>
+          {this.state.dataModel}</Text> 
           <Text style={styles.separation}></Text> 
+          < Text>{this.state.card.pic}</Text>
         </Content>
       </Container>
     );
